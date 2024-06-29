7050a9fe0a09142c8484b5b94adcafe937005006
fwiffo <fwiffo@>
2024-06-29 22:56:27 +0300

    fix accrual host link

--- cmd/gophermart/internal/server/accrual/accrual.go
+++ cmd/gophermart/internal/server/accrual/accrual.go
@@ -3,32 +3,33 @@ package accrual
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/go-resty/resty/v2"
 
+	"github.com/GaryShem/gopher/cmd/gophermart/internal/server/logging"
 	"github.com/GaryShem/gopher/cmd/gophermart/internal/server/storage/repository"
 )
 
 var ErrOrderNotRegistered = errors.New("order not registered")
 var ErrInternalAccrualError = errors.New("internal accrual error")
-var ErrUnreachableError = errors.New("unreachable accrual error")
 
 type BonusTracker struct {
 	Address string
 }
 
 func NewBonusTracker(address string) *BonusTracker {
+	logging.Log.Infoln("will try to address accrual at:", address)
 	return &BonusTracker{Address: address}
 }
 
 func (b *BonusTracker) UpdateOrder(orderID string) (*repository.Order, error) {
 	client := resty.New()
-	url := "http://{host}/api/orders/{number}"
-	request := client.R().
-		SetPathParam("host", b.Address).
-		SetPathParam("number", orderID)
+	url := fmt.Sprintf("%s/api/orders/%s", b.Address, orderID)
+	logging.Log.Infoln("trying to request accrual at url:", url)
+	request := client.R()
 	result := new(repository.Order)
 	for {
 		response, err := request.Get(url)
--- cmd/gophermart/internal/server/config/config.go
+++ cmd/gophermart/internal/server/config/config.go
@@ -16,7 +16,7 @@ func ParseServerFlags() (ServerConfig, error) {
 	result := ServerConfig{}
 	flag.StringVar(&result.RunAddress, "a", "localhost:8080", "service address")
 	flag.StringVar(&result.DBString, "d", "host=localhost user=postgres password=1231 dbname=postgres sslmode=disable", "database URI")
-	flag.StringVar(&result.AccrualAddress, "r", "localhost:8081", "accrual address")
+	flag.StringVar(&result.AccrualAddress, "r", "http://localhost:8081", "accrual address")
 	flag.Parse()
 
 	var ec ServerConfig
